65ec5791a67259b80a302b324e8d421184039ac4
David Abell <david.abell.m@gmail.com>
2022-05-31 19:07:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Board from "./components/Board";
 
 function App() {
+  const [history, setHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+      nextPlayer: "X",
+      winner: false,
+    },
+  ]);
+  const [current, setCurrent] = useState(history.at(-1));
+
+  useEffect(() => {
+    if (current.winner) return;
+    if (isWinner(current.squares)) {
+      console.log("winner");
+      let data = { ...current };
+      data.xIsNext ? (data.winner = "O") : (data.winner = "X");
+      data.winner = true;
+      setCurrent(data);
+    }
+  }, [current]);
+
+  function handleClick(i) {
+    if (current.winner) return;
+    let data = { ...current };
+    data.squares[i] = data.nextPlayer;
+    data.xIsNext ? (data.nextPlayer = "O") : (data.nextPlayer = "X");
+    data.xIsNext = !data.xIsNext;
+    setCurrent(data);
+    setHistory([...history].concat(data));
+  }
+
+  function isWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let line of lines) {
+      const [a, b, c] = line;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const gameStatus = current.winner
+    ? `Player ${current.winner} has Won`
+    : `The next player is: ${current.nextPlayer}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game">
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+          status={gameStatus}
+        />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
     </div>
   );
 }
