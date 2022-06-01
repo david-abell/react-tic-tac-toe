1a5ac6dd4ebddabc2771530c5b850186959bbada
David Abell <david.abell.m@gmail.com>
2022-06-01 15:54:42 +0100

    move state to app and refactor

--- src/App.js
+++ src/App.js
@@ -1,77 +1,90 @@
 import { useState, useEffect } from "react";
 import Board from "./components/Board";
+import "./App.css";
+import { cloneDeep } from "lodash";
 
 function App() {
   const [history, setHistory] = useState([
     {
       squares: Array(9).fill(null),
-      xIsNext: true,
-      nextPlayer: "X",
-      winner: false,
     },
   ]);
-  const [current, setCurrent] = useState(history.at(-1));
+  const [xIsNext, setXIsNext] = useState(true);
+  const [winner, setWinner] = useState(false);
+  const [moveNumber, setMoveNumber] = useState(0);
 
   useEffect(() => {
-    if (current.winner) return;
-    if (isWinner(current.squares)) {
-      console.log("winner");
-      let data = { ...current };
-      data.xIsNext ? (data.winner = "O") : (data.winner = "X");
-      data.winner = true;
-      setCurrent(data);
+    let data = history.at(moveNumber);
+    if (isWinner(data.squares)) {
+      setWinner(xIsNext ? "O" : "X");
     }
-  }, [current]);
+  }, [history, xIsNext, moveNumber]);
 
-  function handleClick(i) {
-    if (current.winner) return;
-    let data = { ...current };
-    data.squares[i] = data.nextPlayer;
-    data.xIsNext ? (data.nextPlayer = "O") : (data.nextPlayer = "X");
-    data.xIsNext = !data.xIsNext;
-    setCurrent(data);
-    setHistory([...history].concat(data));
+  function handleClick(ev) {
+    if (winner || history[moveNumber].squares[ev]) return;
+    let data = cloneDeep(history[moveNumber]);
+    data.squares[ev] = xIsNext ? "X" : "O";
+    let newHistory = [...history];
+    newHistory.push(data);
+    setHistory(newHistory);
+    setXIsNext(!xIsNext);
+    setMoveNumber(moveNumber + 1);
   }
 
-  function isWinner(board) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let line of lines) {
-      const [a, b, c] = line;
-      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-        return true;
-      }
-    }
-    return false;
+  function goToMove(move) {
+    setMoveNumber(move);
+    setHistory(history.slice(0, move + 1));
+    setXIsNext(move % 2 === 0);
   }
 
-  const gameStatus = current.winner
-    ? `Player ${current.winner} has Won`
-    : `The next player is: ${current.nextPlayer}`;
+  const moves = history.map((move, index) => {
+    const moveDescription = index ? `Go to move #${index}` : "Go to game start";
+    return (
+      <li key={`move${index}`}>
+        <button onClick={() => goToMove(index)}>{moveDescription}</button>
+      </li>
+    );
+  });
+
+  const status = winner
+    ? `Player ${winner} has Won`
+    : `The next player is: ${xIsNext ? "X" : "O"}`;
 
   return (
     <div className="game">
       <div className="game-board">
         <Board
-          squares={current.squares}
-          onClick={(i) => handleClick(i)}
-          status={gameStatus}
+          squares={history[moveNumber].squares}
+          onClick={handleClick}
+          status={status}
         />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
+}
+
+function isWinner(board) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 export default App;
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,58 +1,13 @@
 import Square from "./Square";
 
 function Board(props) {
-  // const [squares, setSquares] = useState(Array(9).fill(null));
-  // const [xIsNext, setXisNext] = useState(true);
-  // const [nextPlayer, setNextPlayer] = useState("X");
-  // const [winner, setWinner] = useState(null);
-
-  // useEffect(() => {
-  //   if (checkWinner(squares)) {
-  //     setWinner(xIsNext ? "O" : "X");
-  //   }
-  // }, [squares, xIsNext]);
-
-  // function handleClick(i) {
-  //   if (winner) return;
-  //   const nextSquares = [...squares];
-  //   nextSquares[i] = nextPlayer;
-  //   setNextPlayer(xIsNext ? "O" : "X");
-  //   setSquares(nextSquares);
-  //   setXisNext(!xIsNext);
-  // }
-
   function renderSquare(i) {
     return (
       <Square value={props.squares[i] || i} onClick={() => props.onClick(i)} />
     );
   }
-
-  // function checkWinner(board) {
-  //   const lines = [
-  //     [0, 1, 2],
-  //     [3, 4, 5],
-  //     [6, 7, 8],
-  //     [0, 3, 6],
-  //     [1, 4, 7],
-  //     [2, 5, 8],
-  //     [0, 4, 8],
-  //     [2, 4, 6],
-  //   ];
-  //   for (let line of lines) {
-  //     const [a, b, c] = line;
-  //     if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-  //       return board[a];
-  //     }
-  //   }
-  //   return null;
-  // }
-
-  // const gameStatus = winner
-  //   ? `Player ${winner} has Won`
-  //   : `The next player is: ${nextPlayer}`;
   return (
     <div>
-      <div className="status">{props.status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
